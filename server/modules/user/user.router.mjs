722729e7281261c26fa5231061217ea1bcca95c0
Adi Booker <adib4324@gmail.com>
2024-04-04 10:20:25 +0300

    JOI auth

--- server/modules/user/user.router.mjs
+++ server/modules/user/user.router.mjs
@@ -1,3 +1,4 @@
+import Joi from 'joi'; 
 import raw from "../../middleware/route.async.wrapper.mjs"
 import express from "express"
 import log from '@ajar/marker'
@@ -5,26 +6,36 @@ import user_model from "./user.model.mjs"
 
 const router = express.Router();
 
-// parse json req.body on post routes
+// Define Joi schema for user data
+const userSchema = Joi.object({
+  first_name: Joi.string().required(),
+  last_name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().required(),
+});
+
+// Middleware function to validate req.body using Joi schema
+const validateUserData = (req, res, next) => {
+  const { error } = userSchema.validate(req.body);
+  if (error) {
+    console.error('Joi validation error:', error); 
+    return res.status(400).json({ error: error.details[0].message });
+  }
+  console.log('Joi validation successful'); 
+  next();
+};
+
 router.use(express.json())
 
 // CREATES A NEW USER
-router.post("/", async (req, res,next) => {
-    try{
-      const user = await user_model.create(req.body);
-      res.status(200).json(user);
-    }catch(err){
-      next(err)
-    }
-});
-
-router.post("/", raw(async (req, res) => {
-     log.obj(req.body, "create a user, req.body:");
-     const user = await user_model.create(req.body);
-     res.status(200).json(user);
+router.post("/", validateUserData, raw(async (req, res) => {
+  log.obj(req.body, "create a user, req.body:");
+  const user = await user_model.create(req.body);
+  res.status(200).json(user);
 }));
 
 // GET ALL USERS
+//   http://localhost:3030/api/users
 router.get( "/",raw(async (req, res) => {
     const users = await user_model.find()
                                   // .select(`-_id 
@@ -41,14 +52,14 @@ router.get( "/",raw(async (req, res) => {
 // <*> is a value number first for how many users to be display, and the second for what page to view | do not use the bracets
 // Example  http://localhost:3030/api/users/paginate/10?page=4 
 router.get('/:paginate/:items', async (req, res) => {
-  const users = await user_model.find()
+  // const users = await user_model.find()
   const itemsPerPage = parseInt(req.params.items);
   const currentPage = parseInt(req.query.page);
   const skip = (currentPage - 1) * itemsPerPage;
 
   try {
     const users = await user_model.find()
-      .skip(skip)  // Skip documents based on page number
+      .skip(skip)  
       .limit(itemsPerPage)
       .exec();
     res.json(users);
@@ -58,31 +69,26 @@ router.get('/:paginate/:items', async (req, res) => {
   }
 });
 
+
 // GETS A SINGLE USER
-router.get("/:id",raw(async (req, res) => {
-    const user = await user_model.findById(req.params.id)
-                                    .select(`-_id 
-                                        first_name 
-                                        last_name 
-                                        email
-                                        phone`);
-    if (!user) return res.status(404).json({ status: "No user found." });
-    res.status(200).json(user);
-  })
-);
+///  http://localhost:3030/api/users/:id
+router.get("/:id", raw(async (req, res) => {
+  const user = await user_model.findById(req.params.id);
+  if (!user) return res.status(404).json({ status: "No user found." });
+  res.status(200).json(user);
+}));
+
 // UPDATES A SINGLE USER
-router.put("/:id",raw(async (req, res) => {
-    const user = await user_model.findByIdAndUpdate(req.params.id,req.body, 
-                                                    {new: true, upsert: true });
-    res.status(200).json(user);
-  })
-);
+router.put("/:id", validateUserData, raw(async (req, res) => {
+  const user = await user_model.findByIdAndUpdate(req.params.id, req.body, { new: true, upsert: true });
+  res.status(200).json(user);
+}));
+
 // DELETES A USER
-router.delete("/:id",raw(async (req, res) => {
-    const user = await user_model.findByIdAndRemove(req.params.id);
-    res.status(200).json(user);
-  })
-);
+router.delete("/:id", raw(async (req, res) => {
+  const user = await user_model.findByIdAndRemove(req.params.id);
+  res.status(200).json(user);
+}));
 
 export default router;
 
